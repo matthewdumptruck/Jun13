ea44ff3ab9a95c3c0c336a9d80ad2cd3c8578cd9
Matt Leonard <mattleonard17@gmail.com>
2013-06-13 09:41:31 -0400

    first commit

---
+++ Jun13/Jun13AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun13AppDelegate.h
+//  Jun13
+//
+//  Created by matthewdumptruck on 6/12/13.
+//  Copyright (c) 2013 matthewdumptruck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun13AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
